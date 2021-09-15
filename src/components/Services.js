f866b7b9198b9cabd1e98f4fb56efe9b48103680
ziadoon95 <zaidun10@yahoo.com>
2021-09-15 15:26:19 +0200

    fin-journée Ziadoon

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,20 +7,20 @@ export default function Services() {
                 <h1 class="sections-title">SERVICES</h1>
                 <div class="services flex-container wrap"  id="services-firstchild">
                     <div class="bg-bk flex-item">
-                        <StaticImage src="../images/services-1.png" alt="services-con"
+                        <StaticImage src="../images/services/ux1.png" alt="services-con"
                      
                         />
                         <p class="services-titles">UX design</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="flex-item">
-                        <StaticImage src="../images/services/seo.png" alt="services-con"/>
-                        <p class="services-titles ">UX design</p>
+                        <StaticImage src="../images/services/internet.png" alt="services-con"/>
+                        <p class="services-titles ">Creation site web</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="bg-bk flex-item">
-                        <StaticImage src="../images/services/internet.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <StaticImage src="../images/services/ux (2).png" alt="services-con"/>
+                        <p class="services-titles">Rédaction de contenu</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
             
@@ -28,17 +28,17 @@ export default function Services() {
                 <div class="services flex-container wrap">
                     <div class="flex-item">
                         <StaticImage src="../images/services/ecopense.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <p class="services-titles">Hébergement site web</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="bg-bk flex-item">
                         <StaticImage src="../images/services/hebergement.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <p class="services-titles">Réferencement SEO</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="flex-item">
                         <StaticImage src="../images/services/internet.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <p class="services-titles">Eco-pensée</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                 </div>
