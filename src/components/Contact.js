3536cc5a66534523732636885afdd5153c95d0d4
ziadoon95 <zaidun10@yahoo.com>
2021-09-15 11:07:59 +0200

    15-9

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import { StaticImage } from 'gatsby-plugin-image'
+import styles from "../style/stlye.css"
+
 
 export default function Contact() {
     return (
         <section id="contact-page">
         <div id="contact-header-photo">
-            <StaticImage src="../images/contact-header.png" alt=""/>
+            <StaticImage src="../images/contact-header.png" alt=""  
+            
+            style={{width : '100%'}}
+      height={200}
+        
+      />
         </div>
         <div id="contact-aside-form-container">
             <div id="contact-form-container">
--- src/components/Services.js
+++ src/components/Services.js
@@ -7,7 +7,9 @@ export default function Services() {
                 <h1 class="sections-title">SERVICES</h1>
                 <div class="services flex-container wrap"  id="services-firstchild">
                     <div class="bg-bk flex-item">
-                        <StaticImage src="../images/services-1.png" alt="services-con"/>
+                        <StaticImage src="../images/services-1.png" alt="services-con"
+                     
+                        />
                         <p class="services-titles">UX design</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
