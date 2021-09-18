672936d761a68a0b8599ede9231472655716304a
ziadoon95 <zaidun10@yahoo.com>
2021-09-19 00:08:47 +0200

    update service

--- src/components/Services.js
+++ src/components/Services.js
@@ -7,36 +7,36 @@ export default function Services() {
                 <h1 class="sections-title" id="service-title">SERVICES</h1>
                 <div class="services flex-container wrap"  id="services-firstchild">
                     <div class="bg-bk flex-item">
-                        <StaticImage src="../images/services-1.png" alt="services-con"
+                        <StaticImage src="../images/services/ux.png" width={80} className="services-img" alt="services-con"
                      
                         />
                         <p class="services-titles">UX design</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="flex-item">
-                        <StaticImage src="../images/services/seo.png" alt="services-con"/>
-                        <p class="services-titles ">UX design</p>
+                        <StaticImage src="../images/services/internet.png" width={90} className="services-img" alt="services-con"/>
+                        <p class="services-titles ">Création site internet</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="bg-bk flex-item">
-                        <StaticImage src="../images/services/internet.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <StaticImage src="../images/services-1.png" width={90} className="services-img" alt="services-con"/>
+                        <p class="services-titles">Rédaction de contenu</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     {/* ajout ligne */}
                     <div class="flex-item">
-                        <StaticImage src="../images/services/ecopense.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <StaticImage src="../images/services/hebergement.png" className="services-img" alt="services-con"/>
+                        <p class="services-titles">Hébergement site web</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="bg-bk flex-item">
-                        <StaticImage src="../images/services/hebergement.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <StaticImage src="../images/services/seo.png" width={90} className="services-img" alt="services-con"/>
+                        <p class="services-titles">Réferencement SEO</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
                     <div class="flex-item">
-                        <StaticImage src="../images/services/internet.png" alt="services-con"/>
-                        <p class="services-titles">UX design</p>
+                        <StaticImage src="../images/services/ecopense.png" className="services-img" alt="services-con"/>
+                        <p class="services-titles">Eco-conception</p>
                         <p class="services-description sections-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit suspendisse amet, hendrerit sed turpis commodo, nulla amet, quisque.</p>
                     </div>
             
