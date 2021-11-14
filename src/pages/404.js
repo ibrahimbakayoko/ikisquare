7f9e1fab8ee385e5a686669e2926b450d81bb1da
Ziadoon AL-OBAIDI <zaidun10@yahoo.com>
2021-11-14 11:22:01 +0100

    update

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,10 +2,8 @@ import { useEffect } from 'react';
 import { navigate } from "@reach/router"
 
 export default () => {
-  
-    if (typeof window !== 'undefined') {
-        window.location = '/';
-      }
-  
+  useEffect(() => {
+    navigate('/');
+  }, []);
   return null;
 };
